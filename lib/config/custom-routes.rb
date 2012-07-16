222c034944eaf8b542a6c128131cef181d01e281
Seb Bacon <seb.bacon@gmail.com>
2012-07-16 10:30:35 +0100

    Don't use dispatcher/reloading trick with routes - it won't work.

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,17 +1,13 @@
 # Here you can override or add to the pages in the core website
 
-
-require 'dispatcher'
-Dispatcher.to_prepare do
-    ActionController::Routing::Routes.draw do |map|
-        # brand new controller example
-        map.with_options :controller => 'general' do |general|
-            general.mycontroller '/mycontroller', :action => 'mycontroller'
-        end    
-
-        # Additional help page example
-        map.with_options :controller => 'help' do |help|
-            help.help_out '/help/help_out', :action => 'help_out'
-        end
+ActionController::Routing::Routes.draw do |map|
+    # brand new controller example
+    map.with_options :controller => 'general' do |general|
+        general.mycontroller '/mycontroller', :action => 'mycontroller'
+    end    
+    
+    # Additional help page example
+    map.with_options :controller => 'help' do |help|
+        help.help_out '/help/help_out', :action => 'help_out'
     end
 end
