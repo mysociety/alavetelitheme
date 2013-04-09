33077ea01dc1317c4fce118889e9e8fdcaacb4b1
Mark Longair <mhl@pobox.com>
2013-04-09 17:14:35 +0100

    Update the routing for Rails 3

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,13 +1,8 @@
 # Here you can override or add to the pages in the core website
 
-ActionController::Routing::Routes.draw do |map|
+Rails.application.routes.draw do
     # brand new controller example
-    map.with_options :controller => 'general' do |general|
-        general.mycontroller '/mycontroller', :action => 'mycontroller'
-    end    
-    
+    match '/mycontroller' => 'general#mycontroller'
     # Additional help page example
-    map.with_options :controller => 'help' do |help|
-        help.help_out '/help/help_out', :action => 'help_out'
-    end
+    match '/help/help_out' => 'help#help_out'
 end
