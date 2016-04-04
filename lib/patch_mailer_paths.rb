8ccab37ebc69e0e729b0b1748378006a811f4071
Louise Crow <louise.crow@gmail.com>
2016-04-04 15:05:20 +0100

    Convert to two-space indentation.

--- lib/patch_mailer_paths.rb
+++ lib/patch_mailer_paths.rb
@@ -3,8 +3,8 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
-    # Override mailer templates with theme ones. Note doing this in a before_filter,
-    # as we do with the controller paths, doesn't seem to have any effect when
-    # running in production
-    ActionMailer::Base.prepend_view_path File.join(File.dirname(__FILE__), "views")
+  # Override mailer templates with theme ones. Note doing this in a before_filter,
+  # as we do with the controller paths, doesn't seem to have any effect when
+  # running in production
+  ActionMailer::Base.prepend_view_path File.join(File.dirname(__FILE__), "views")
 end
