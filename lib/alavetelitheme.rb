9e8583fdf3dec2a69ab7df2d7ed4809f17085293
Gareth Rees <gareth@garethrees.co.uk>
2016-09-29 17:36:38 +0100

    debug

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -41,6 +41,7 @@ $alaveteli_route_extensions << 'custom-routes.rb'
                                'app',
                                'assets',
                                asset_type)
+  puts "PREPENDING #{theme_asset_path}"
   Rails.application.config.assets.prepend_path theme_asset_path
   #Rails.application.config.sass.load_paths.unshift theme_asset_path
 end
@@ -51,8 +52,12 @@ end
                                'assets',
                                'stylesheets',
                                'responsive')
+puts "PREPENDING #{responsive_stylesheet_path}"
 Rails.application.config.assets.prepend_path responsive_stylesheet_path
-                              
+
+puts "PATHS:"
+puts Rails.application.config.assets
+
 #puts "Adding #{responsive_stylesheet_path} to sass load path"
 #Rails.application.config.sass.load_paths.unshift responsive_stylesheet_path
 #puts "Sass load path #{Rails.application.config.sass.load_paths}"
