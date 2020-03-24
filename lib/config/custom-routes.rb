62150e2ffc6674d365a62fa45fa1c87a965c962f
Ellewe <ellelingh@hotmail.se>
2020-03-24 17:23:38 +0100

    Update custom-routes.rb

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,11 +1,11 @@
--*- encoding : utf-8 -*-
+# -*- encoding : utf-8 -*-
 # Here you can override or add to the pages in the core website
 
 #Rails.application.routes.draw do
  # get '/help/nonprofits' => 'help#nonprofits'
 #end
 
--*- encoding : utf-8 -*-
+# -*- encoding : utf-8 -*-
 # Here you can override or add to the pages in the core website
 
 # Rails.application.routes.draw do
@@ -13,27 +13,4 @@
 # end
 
 
-  get '/help/companies'  => 'help#companies',
-    :via => 'get',
-    :as => 'help_companies'
 
-  get '/help/authorities'  => 'help#authorities',
-    :via => 'get',
-    :as => 'help_authorities'
-
-  get '/help/nonprofits'  => 'help#nonprofits',
-    :via => 'get',
-    :as => 'help_nonprofits'
-
-  get '/help/citizens'  => 'help#citizens',
-    :via => 'get',
-    :as => 'help_citizens'
-
-  get '/help/merch'  => 'help#merch',
-    :via => 'get',
-    :as => 'help_merch'
-
-  get '/help/donate'  => 'help#donate',
-    :via => 'get',
-    :as => 'help_donate'
-end
