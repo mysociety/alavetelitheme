bf805d854c1e98c8c97012b76f2f084954cbf45f
Mattias Axell <13077275+mattiasaxell@users.noreply.github.com>
2022-02-09 18:12:50 +0100

    Add terms for page terms of use

--- lib/config/custom-routes.rb
+++ lib/config/custom-routes.rb
@@ -1,16 +1,8 @@
 # -*- encoding : utf-8 -*-
 # Here you can override or add to the pages in the core website
 
-#Rails.application.routes.draw do
- # get '/help/nonprofits' => 'help#nonprofits'
-#end
-
-# -*- encoding : utf-8 -*-
-# Here you can override or add to the pages in the core website
-
-# Rails.application.routes.draw do
-  # get '/help/nonprofits' => 'help#nonprofits'
-# end
-
-
-
+Rails.application.routes.draw do
+# brand new controller for terms of use
+   get '/help/terms' => 'help#terms',
+      as: :help_terms
+end
