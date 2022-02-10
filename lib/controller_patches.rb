e34d9cd7ba13c115f9a18ceaa4b0c94b6ae14ffb
Mattias Axell <13077275+mattiasaxell@users.noreply.github.com>
2022-02-10 20:22:33 +0100

    Fix t typo

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -5,7 +5,7 @@
 # classes are reloaded, but initialization is not run each time.
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
-tRails.configuration.to_prepare do
+Rails.configuration.to_prepare do
   # Example adding an instance variable to the frontpage controller
   # GeneralController.class_eval do
   #   def mycontroller
